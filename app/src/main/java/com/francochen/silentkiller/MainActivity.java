26cc9225c06d726d5bddf528ff5f89e401d1bfe7
Franco Chen <francochen2000@hotmail.com>
2020-01-25 19:01:53 -0500

    Retrofit

--- app/src/main/java/com/francochen/silentkiller/MainActivity.java
+++ app/src/main/java/com/francochen/silentkiller/MainActivity.java
@@ -1,14 +1,21 @@
 package com.francochen.silentkiller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+    private Retrofit retrofit = new Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("localhost:5000")
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
