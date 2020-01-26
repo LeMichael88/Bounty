bcaf4acc35d2f403a6ef4f2b9b3399fb14ecb525
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-01-25 20:49:40 -0500

    Add lobby UI

--- app/src/main/java/com/francochen/silentkiller/MainActivity.java
+++ app/src/main/java/com/francochen/silentkiller/MainActivity.java
@@ -3,6 +3,9 @@ package com.francochen.silentkiller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = findViewById(R.id.grid_view);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(gridView.getContext());
+
+        LinearLayout player = findViewById(R.id.player);
+
     }
 }
